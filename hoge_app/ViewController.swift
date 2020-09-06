2195525a29d275fefb4c8ac6f8d896257ccf0fdf
soymd <pacificstate33@gmial.com>
2020-09-06 13:56:40 +0900

    Initial Commit

---
+++ hoge_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hoge_app
+//
+//  Created by soymd on 2020/09/06.
+//  Copyright © 2020 soymd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
